c213f99992a44951cccf7f8fb804f906a9318b4c
juliak321 <juliakomolova964@gmail.com>
2026-03-15 12:44:33 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Julia Komolova" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! I'm Julia, a sophmore from New Jersey. I love math and physics, and want to pursue either mechanical or electrical engineering. However, I have had limited exposure to the field of electrical engineering, and hope to learn more through BWSI! I have a huge amount of hobbies, but to list a couple, I enjoy playing piano, badminton, tennis, reading/writing, and especially arts/crafts. As of right now, I'm working to add CAD and Arduino to the mix, and I have been having a lot of fun!")
 
 if __name__ == "__main__":
     main()
